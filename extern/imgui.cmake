f36b25b7d7248e8ca6cea30f34eba247315ba28a
Dolfun <abhinav150104@gmail.com>
2024-03-07 15:33:09 +0530

    Add glad and imgui

---
+++ extern/imgui.cmake
@@ -0,0 +1,19 @@
+add_subdirectory(glfw)
+add_subdirectory(glad)
+
+set(imgui_sources
+  imgui/imgui.cpp
+  imgui/imgui_demo.cpp
+  imgui/imgui_draw.cpp
+  imgui/imgui_tables.cpp
+  imgui/imgui_widgets.cpp
+  imgui/backends/imgui_impl_glfw.cpp
+  imgui/backends/imgui_impl_opengl3.cpp
+  imgui/misc/cpp/imgui_stdlib.cpp
+)
+
+set(imgui_headers imgui imgui/backends imgui/misc/cpp)
+
+add_library(imgui ${imgui_sources})
+target_link_libraries(imgui PRIVATE glfw glad)
+target_include_directories(imgui PUBLIC ${imgui_headers})
