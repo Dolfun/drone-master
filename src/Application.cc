5001d305ae9415c290e5ec1068e18774a69acb48
Dolfun <abhinav150104@gmail.com>
2024-03-17 03:37:24 +0530

    Remove asio

--- src/Application.cc
+++ src/Application.cc
@@ -6,24 +6,7 @@
 
 Application::Application(const ApplicationInfo& info) : 
   window_manager(info.window_x, info.window_y, info.fullscreen),
-  user_interface(window_manager.handle(), info.font_name),
-  io_context_thread(&Application::io_context_run, this),
-  server(io_context, info.port_no) {}
-
-Application::~Application() {
-  io_context.stop();
-}
-
-void Application::io_context_run() {
-  auto work_guard = asio::make_work_guard(io_context);
-
-  try {
-    io_context.run();
-  } catch (const std::exception& e) {
-    std::cerr << "asio: " << e.what() << '\n';
-    std::terminate();
-  }
-}
+  user_interface(window_manager.handle(), info.font_name) {}
 
 void Application::run() {
   auto window = window_manager.handle();
--- src/Application.h
+++ src/Application.h
@@ -2,28 +2,14 @@
 #include "ApplicationInfo.h"
 #include "UI/WindowManager.h"
 #include "UI/UserInterface.h"
-#include "Server.h"
-#include <thread>
 
 class Application {
 public:
   Application(const ApplicationInfo&);
 
-  Application(const Application&) = delete;
-  Application& operator= (const Application&) = delete;
-  Application(Application&&) = delete;
-  Application& operator= (Application&&) = delete;
-
-  ~Application();
-
   void run();
 
 private:
-  void io_context_run();
-
   WindowManager window_manager;
   UserInteface user_interface;
-  asio::io_context io_context;
-  std::jthread io_context_thread;
-  Server server;
 };
