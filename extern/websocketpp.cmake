0ff1af36a1753cffbf550ee63d22df7a97b01e42
Dolfun <abhinav150104@gmail.com>
2024-03-17 15:52:14 +0530

    Add c++11 flag to websocket target

--- extern/websocketpp.cmake
+++ extern/websocketpp.cmake
@@ -1,3 +1,4 @@
 add_library(websocketpp INTERFACE)
 target_include_directories(websocketpp INTERFACE "${CMAKE_CURRENT_SOURCE_DIR}/websocketpp")
 target_compile_features(websocketpp INTERFACE cxx_std_20)
+target_compile_definitions(websocketpp INTERFACE _WEBSOCKETPP_CPP11_STL_)
